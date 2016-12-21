fd63c689f2c34397228cec7d7cc9ccf69600bf80
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-12-21 18:42:32 +0200

    transfer views to jade templates add post methods for signup, signin and settings menu

--- server.js
+++ server.js
@@ -3,12 +3,39 @@
  */
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public');
+    //res.sendFile(__dirname + '/public');
+    res.render('index.jade', {});
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.get('/signup', function (req, res) {
+    res.render('signup.jade', {});
+});
+
+app.post('/signup', function (req, res) {
+    res.send(req.body);
+});
+
+app.get('/signin', function (req, res) {
+    res.render('signin.jade', {});
+});
+
+app.post('/signin', function (req, res) {
+    res.send(req.body);
+});
+
+app.get('/settings', function (req, res) {
+    res.render('settings.jade', {});
+});
+
+app.listen(process.env.PORT || 3000, function () {
     console.log('Server start. Listening on port 3000')
 });
