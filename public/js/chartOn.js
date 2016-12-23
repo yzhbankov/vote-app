63cfef6c838f53e25cf68b14eb17993471bb54c2
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-12-23 23:30:39 +0200

    add pie chart

---
+++ public/js/chartOn.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Iaroslav Zhbankov on 23.12.2016.
+ */
+var ctx = document.getElementById("myChart").getContext("2d");
+ctx.canvas.width = 100;
+ctx.canvas.height = 100;
+alert(optionsName);
+var data = {
+    labels: optionsName,
+    datasets: [
+        {
+            data: optionsSize,
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56",
+                "#FFCE56",
+                "F4FF79",
+                "6DB3FF",
+                "00C900",
+                "1E2C3D"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56",
+                "#FFCE56",
+                "F4FF79",
+                "6DB3FF",
+                "00C900",
+                "1E2C3D"
+            ]
+        }]
+};
+var options = {
+    cutoutPercentage: 0,
+    rotation: -0.5 * Math.PI,
+    circumference: 2 * Math.PI,
+    animation: {
+        animateRotate: true,
+        animateScale: false
+    }
+
+};
+Chart.defaults.global.maintainAspectRatio = false;
+var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: data,
+    options: options
+});
